f9f027d5a7abfb23ac2941eb055d523d18a14ea5
Benjamin Kott <benjamin.kott@outlook.com>
2024-10-15 11:40:36 +0200

    [TASK] Migrate element icons to svg

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -1,31 +1,31 @@
 <?php
 declare(strict_types=1);
 
-use TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider;
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 
 return [
     'icon_intro' => [
-        'provider' => BitmapIconProvider::class,
-        'source' => 'EXT:theme_portfolio/Resources/Public/Icons/Backend/intro.png',
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:theme_portfolio/Resources/Public/Icons/Backend/intro.svg',
     ],
     'icon_skills' => [
-        'provider' => BitmapIconProvider::class,
-        'source' => 'EXT:theme_portfolio/Resources/Public/Icons/Backend/skills.png',
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:theme_portfolio/Resources/Public/Icons/Backend/skills.svg',
     ],
     'icon_resume' => [
-        'provider' => BitmapIconProvider::class,
-        'source' => 'EXT:theme_portfolio/Resources/Public/Icons/Backend/resume.png',
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:theme_portfolio/Resources/Public/Icons/Backend/resume.svg',
     ],
     'icon_services' => [
-        'provider' => BitmapIconProvider::class,
-        'source' => 'EXT:theme_portfolio/Resources/Public/Icons/Backend/services.png',
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:theme_portfolio/Resources/Public/Icons/Backend/services.svg',
     ],
     'icon_clients' => [
-        'provider' => BitmapIconProvider::class,
-        'source' => 'EXT:theme_portfolio/Resources/Public/Icons/Backend/clients.png',
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:theme_portfolio/Resources/Public/Icons/Backend/clients.svg',
     ],
     'icon_project' => [
-        'provider' => BitmapIconProvider::class,
-        'source' => 'EXT:theme_portfolio/Resources/Public/Icons/Backend/project.png',
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:theme_portfolio/Resources/Public/Icons/Backend/project.svg',
     ],
 ];
